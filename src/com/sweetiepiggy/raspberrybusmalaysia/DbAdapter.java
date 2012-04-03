52d1705b5debb1218d88076e57676d6be79f3b17
Sweetie Piggy <sweetiepiggyapps@gmail.com>
2012-04-03 18:42:06 +0800

    clean up whitespace

--- src/com/sweetiepiggy/raspberrybusmalaysia/DbAdapter.java
+++ src/com/sweetiepiggy/raspberrybusmalaysia/DbAdapter.java
@@ -28,67 +28,67 @@ import android.util.Log;
 
 
 public class DbAdapter {
-    public static final String KEY_FROM_CITY = "from_city";
-    public static final String KEY_TO_CITY = "to_city";
-    public static final String KEY_ROWID = "_id";
+	public static final String KEY_FROM_CITY = "from_city";
+	public static final String KEY_TO_CITY = "to_city";
+	public static final String KEY_ROWID = "_id";
 
-    private static final String TAG = "DbAdapter";
-    private DatabaseHelper mDbHelper;
-    private SQLiteDatabase mDb;
+	private static final String TAG = "DbAdapter";
+	private DatabaseHelper mDbHelper;
+	private SQLiteDatabase mDb;
 
-    private static final String DATABASE_CREATE =
-        "create table notes (_id integer primary key autoincrement, " +
+	private static final String DATABASE_CREATE =
+		"create table notes (_id integer primary key autoincrement, " +
 	KEY_FROM_CITY + " text not null, " +
 	KEY_TO_CITY + " text not null);";
 
-    private static final String DATABASE_NAME = "bus";
-    private static final String DATABASE_TABLE = "trips";
-    private static final int DATABASE_VERSION = 1;
+	private static final String DATABASE_NAME = "bus";
+	private static final String DATABASE_TABLE = "trips";
+	private static final int DATABASE_VERSION = 1;
 
-    private final Context mCtx;
+	private final Context mCtx;
 
-    private static class DatabaseHelper extends SQLiteOpenHelper {
+	private static class DatabaseHelper extends SQLiteOpenHelper {
 
-        DatabaseHelper(Context context) {
-            super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        }
+		DatabaseHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
 
-        @Override
-        public void onCreate(SQLiteDatabase db) {
-            db.execSQL(DATABASE_CREATE);
-        }
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL(DATABASE_CREATE);
+		}
 
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
-                    + newVersion + ", which will destroy all old data");
-            db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
-            onCreate(db);
-        }
-    }
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+					+ newVersion + ", which will destroy all old data");
+			db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+			onCreate(db);
+		}
+	}
 
-    public DbAdapter(Context ctx) {
-        this.mCtx = ctx;
-    }
+	public DbAdapter(Context ctx) {
+		this.mCtx = ctx;
+	}
 
-    public DbAdapter open() throws SQLException {
-        mDbHelper = new DatabaseHelper(mCtx);
-        mDb = mDbHelper.getWritableDatabase();
-        return this;
-    }
+	public DbAdapter open() throws SQLException {
+		mDbHelper = new DatabaseHelper(mCtx);
+		mDb = mDbHelper.getWritableDatabase();
+		return this;
+	}
 
-    public void close() {
-        mDbHelper.close();
-    }
+	public void close() {
+		mDbHelper.close();
+	}
 
-     /** @return row_id or -1 if failed */
-    public long create_trip(String from_city, String to_city) {
-        ContentValues initial_values = new ContentValues();
-        initial_values.put(KEY_FROM_CITY, from_city);
-        initial_values.put(KEY_TO_CITY, to_city);
+	/** @return row_id or -1 if failed */
+	public long create_trip(String from_city, String to_city) {
+		ContentValues initial_values = new ContentValues();
+		initial_values.put(KEY_FROM_CITY, from_city);
+		initial_values.put(KEY_TO_CITY, to_city);
 
-        return mDb.insert(DATABASE_TABLE, null, initial_values);
-    }
+		return mDb.insert(DATABASE_TABLE, null, initial_values);
+	}
 
 }
 
