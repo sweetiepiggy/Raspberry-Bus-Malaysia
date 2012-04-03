f2475ddc3c5fe2ab79e9c0d317f92783d5853602
Sweetie Piggy <sweetiepiggyapps@gmail.com>
2012-04-03 18:45:12 +0800

    clean up whitespace

--- src/com/sweetiepiggy/raspberrybusmalaysia/RaspberryBusMalaysiaActivity.java
+++ src/com/sweetiepiggy/raspberrybusmalaysia/RaspberryBusMalaysiaActivity.java
@@ -23,10 +23,10 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class RaspberryBusMalaysiaActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 }
