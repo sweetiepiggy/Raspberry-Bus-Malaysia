b4f1d981008acd8ecfa0e5af911e319fea837f74
Sweetie Piggy <sweetiepiggyapps@gmail.com>
2012-05-15 17:41:35 +0800

    add About menu to display license

--- src/com/sweetiepiggy/raspberrybusmalaysia/RaspberryBusMalaysiaActivity.java
+++ src/com/sweetiepiggy/raspberrybusmalaysia/RaspberryBusMalaysiaActivity.java
@@ -22,9 +22,10 @@ package com.sweetiepiggy.raspberrybusmalaysia;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.TextView;
 
 public class RaspberryBusMalaysiaActivity extends Activity
@@ -63,6 +64,25 @@ public class RaspberryBusMalaysiaActivity extends Activity
 				startActivity(intent);
 			}
 		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.about:
+			Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
 
