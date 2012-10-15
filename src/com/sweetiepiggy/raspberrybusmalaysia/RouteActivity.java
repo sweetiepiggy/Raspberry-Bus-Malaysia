0cce0103af5f41f2fd2850e7b323d83db78b8724
Sweetie Piggy <sweetiepiggyapps@gmail.com>
2012-10-15 17:09:31 +0800

    merged RouteResultActivity into RouteActivity

--- src/com/sweetiepiggy/raspberrybusmalaysia/RouteActivity.java
+++ src/com/sweetiepiggy/raspberrybusmalaysia/RouteActivity.java
@@ -19,17 +19,25 @@
 
 package com.sweetiepiggy.raspberrybusmalaysia;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class RouteActivity extends Activity
 {
@@ -37,6 +45,7 @@ public class RouteActivity extends Activity
 	private String m_to_city = "";
 
 	private DbAdapter mDbHelper;
+	private ArrayList<TableRow> m_rows = new ArrayList<TableRow>();
 
 	/** Called when the activity is first created. */
 	@Override
@@ -48,8 +57,6 @@ public class RouteActivity extends Activity
 		mDbHelper = new DbAdapter();
 		mDbHelper.open(this);
 		fill_data();
-
-		init_submit_button();
 	}
 
 	@Override
@@ -95,6 +102,8 @@ public class RouteActivity extends Activity
 							View selected_item, int pos,
 							long id) {
 						m_to_city = ((Cursor)parent.getItemAtPosition(pos)).getString(1);
+						init_labels(m_from_city, m_to_city);
+						sort_by_avg_time(m_from_city, m_to_city);
 					}
 					@Override
 					public void onNothingSelected(AdapterView<?> parentView) {
@@ -120,26 +129,197 @@ public class RouteActivity extends Activity
 		}
 	}
 
-	private void init_submit_button()
+	private void init_labels(final String from_city, final String to_city)
 	{
-		Button submit_button = (Button) findViewById(R.id.submit_button);
-		submit_button.setOnClickListener(new View.OnClickListener() {
+		TextView company = (TextView) findViewById(R.id.company);
+		company.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v)
 			{
-				if (m_from_city.length() == 0) {
-					Toast.makeText(getApplicationContext(), "Select \"From City\"", Toast.LENGTH_SHORT).show();
-				} else if (m_to_city.length() == 0) {
-					Toast.makeText(getApplicationContext(), "Select \"To City\"", Toast.LENGTH_SHORT).show();
-				} else {
-					Intent intent = new Intent(getApplicationContext(), RouteResultActivity.class);
-					Bundle b = new Bundle();
-					b.putString("from_city", m_from_city);
-					b.putString("to_city", m_to_city);
-					intent.putExtras(b);
-					startActivity(intent);
-				}
+				sort_by_company(from_city, to_city);
 			}
 		});
+
+		TextView avg_time = (TextView) findViewById(R.id.avg_time);
+		avg_time.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v)
+			{
+				sort_by_avg_time(from_city, to_city);
+			}
+		});
+
+		TextView avg_delay = (TextView) findViewById(R.id.avg_delay);
+		avg_delay.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v)
+			{
+				sort_by_delay(from_city, to_city);
+			}
+		});
+
+		TextView num_trips = (TextView) findViewById(R.id.num_trips);
+		num_trips.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v)
+			{
+				sort_by_num_trips(from_city, to_city);
+			}
+		});
+	}
+
+	private void sort_by_company(String from_city, String to_city)
+	{
+		Cursor c = mDbHelper.fetch_avg_by_company_sort_company(from_city, to_city);
+		startManagingCursor(c);
+		print_rows(c, from_city, to_city);
+	}
+
+	private void sort_by_avg_time(String from_city, String to_city)
+	{
+		Cursor c = mDbHelper.fetch_avg_by_company(from_city, to_city);
+		startManagingCursor(c);
+		print_rows(c, from_city, to_city);
+
+	}
+
+	private void sort_by_delay(String from_city, String to_city)
+	{
+		Cursor c = mDbHelper.fetch_avg_by_company_sort_delay(from_city, to_city);
+		startManagingCursor(c);
+		print_rows(c, from_city, to_city);
+	}
+
+	private void sort_by_num_trips(String from_city, String to_city)
+	{
+		Cursor c = mDbHelper.fetch_avg_by_company_sort_trips(from_city, to_city);
+		startManagingCursor(c);
+		print_rows(c, from_city, to_city);
+	}
+
+	private void print_rows(Cursor c, String from_city, String to_city)
+	{
+		clear_rows();
+		if (c.moveToFirst()) do {
+			String company = c.getString(c.getColumnIndex(DbAdapter.KEY_COMP));
+			String avg = format_time(c.getInt(c.getColumnIndex(DbAdapter.AVG_TIME)));
+			String delay = format_time_min(c.getInt(c.getColumnIndex(DbAdapter.AVG_DELAY)));
+			String count = c.getString(c.getColumnIndex(DbAdapter.NUM_TRIPS));
+			print_row(company, avg, delay, count, from_city,
+					to_city);
+		} while (c.moveToNext());
+	}
+
+	private String format_time(int time)
+	{
+		String negative = "";
+		if (time < 0) {
+			negative = "-";
+			time *= -1;
+		}
+
+		int hr = time / 3600;
+		time -= hr * 3600;
+		int min = time / 60;
+		return String.format("%s%d%s %02d%s", negative, hr, getResources().getString(R.string.hour_abbr),
+				min, getResources().getString(R.string.minute_abbr));
+	}
+
+	private void print_row(String company, String avg, String delay,
+			String count, String from_city, String to_city)
+	{
+		String display_company = company;
+		if (company.length() > 15) {
+//			company = company.substring(0, 15);
+			display_company = company.replace(' ', '\n');
+		} else if (company.length() == 0) {
+			display_company = getResources().getString(R.string.unknown);
+//			return;
+		}
+
+		Button company_view = new Button(getApplicationContext());
+		TextView avg_view = new TextView(getApplicationContext());
+		TextView delay_view = new TextView(getApplicationContext());
+		Button count_view = new Button(getApplicationContext());
+		company_view.setText(display_company);
+		avg_view.setText(avg);
+		delay_view.setText(delay);
+		count_view.setText(count);
+
+		company_view.setGravity(Gravity.CENTER);
+		avg_view.setGravity(Gravity.CENTER);
+		delay_view.setGravity(Gravity.CENTER);
+		count_view.setGravity(Gravity.CENTER);
+
+		init_company_button(company_view, company);
+		init_count_button(count_view, company, from_city, to_city);
+
+		TableRow tr = new TableRow(getApplicationContext());
+		m_rows.add(tr);
+		tr.addView(company_view);
+		tr.addView(avg_view);
+		tr.addView(delay_view);
+		tr.addView(count_view);
+
+		TableLayout results_layout = (TableLayout) findViewById(R.id.results_layout);
+		results_layout.addView(tr);
+		tr.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_in));
+	}
+
+	private void init_company_button(Button b, final String company)
+	{
+		b.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v)
+			{
+				Intent intent = new Intent(getApplicationContext(),
+					CompanyResultActivity.class);
+				Bundle b = new Bundle();
+				b.putString("company", company);
+				intent.putExtras(b);
+				startActivity(intent);
+			}
+		});
+
+	}
+
+	private void init_count_button(Button b, final String company,
+			final String from_city, final String to_city)
+	{
+		b.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v)
+			{
+				Intent intent = new Intent(getApplicationContext(),
+					TripsActivity.class);
+				Bundle b = new Bundle();
+				b.putString("company", company);
+				b.putString("from_city", from_city);
+				b.putString("to_city", to_city);
+				intent.putExtras(b);
+				startActivity(intent);
+			}
+		});
+	}
+
+	private void clear_rows()
+	{
+		ViewGroup results_layout = (ViewGroup) findViewById(R.id.results_layout);
+		Iterator itr = m_rows.iterator();
+		while (itr.hasNext()) {
+			TableRow row = (TableRow) itr.next();
+//			row.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_out));
+//			row.setVisibility(View.INVISIBLE);
+			results_layout.removeView(row);
+		}
+		m_rows.clear();
+	}
+
+	/* TODO: move format_time() and format_time_min() to their own class */
+	private String format_time_min(int time)
+	{
+		String negative = "";
+		if (time < 0) {
+			negative = "-";
+			time *= -1;
+		}
+
+		int min = time / 60;
+		return String.format("%s%d%s", negative, min, getResources().getString(R.string.minute_abbr));
 	}
 }
 
