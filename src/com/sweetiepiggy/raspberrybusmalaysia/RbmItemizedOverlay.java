0f3d3ce9f19e5313f1bda444630fc142e0bcae4a
Sweetie Piggy <sweetiepiggyapps@gmail.com>
2012-10-31 21:47:41 +0800

    added overlays to map

---
+++ src/com/sweetiepiggy/raspberrybusmalaysia/RbmItemizedOverlay.java
@@ -0,0 +1,66 @@
+/*
+    Copyright (C) 2012 Sweetie Piggy Apps <sweetiepiggyapps@gmail.com>
+
+    This file is part of Raspberry Bus Malaysia.
+
+    Raspberry Bus Malaysia is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    Raspberry Bus Malaysia is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Raspberry Bus Malaysia; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+package com.sweetiepiggy.raspberrybusmalaysia;
+
+import java.util.ArrayList;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class RbmItemizedOverlay extends ItemizedOverlay
+{
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	private Context mContext;
+
+	public RbmItemizedOverlay(Drawable defaultMarker, Context context) {
+		super(boundCenterBottom(defaultMarker));
+		mContext = context;
+	}
+
+	public void addOverlay(OverlayItem overlay) {
+		mOverlays.add(overlay);
+		populate();
+	}
+
+	@Override
+	protected OverlayItem createItem(int i) {
+		return mOverlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		return mOverlays.size();
+	}
+
+	@Override
+	protected boolean onTap(int index) {
+		OverlayItem item = mOverlays.get(index);
+		AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+		dialog.setTitle(item.getTitle());
+		dialog.setMessage(item.getSnippet());
+		dialog.show();
+		return true;
+	}
+}
+
--- src/com/sweetiepiggy/raspberrybusmalaysia/RbmMapActivity.java
+++ src/com/sweetiepiggy/raspberrybusmalaysia/RbmMapActivity.java
@@ -19,10 +19,16 @@
 
 package com.sweetiepiggy.raspberrybusmalaysia;
 
+import java.util.List;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class RbmMapActivity extends MapActivity {
 
@@ -33,6 +39,48 @@ public class RbmMapActivity extends MapActivity {
 		setContentView(R.layout.map);
 		MapView mapView = (MapView) findViewById(R.id.mapview);
 		mapView.setBuiltInZoomControls(true);
+
+		List<Overlay> mapOverlays = mapView.getOverlays();
+		Drawable drawable = getResources().getDrawable(R.drawable.ic_launcher);
+		RbmItemizedOverlay itemizedoverlay = new RbmItemizedOverlay(drawable, this);
+
+		GeoPoint sg_nibong_pt = new GeoPoint(5343523,100300453);
+		OverlayItem sg_nibong_overlay = new OverlayItem(sg_nibong_pt, "Sg Nibong", "Penang");
+		itemizedoverlay.addOverlay(sg_nibong_overlay);
+
+		GeoPoint komtar_pt = new GeoPoint(5414478,100331887);
+		OverlayItem komtar_overlay = new OverlayItem(komtar_pt, "Komtar", "Penang");
+		itemizedoverlay.addOverlay(komtar_overlay);
+
+		GeoPoint vistana_pt = new GeoPoint(5336945,100291939);
+		OverlayItem vistana_overlay = new OverlayItem(vistana_pt, "Vistana Hotel", "Penang");
+		itemizedoverlay.addOverlay(vistana_overlay);
+
+		GeoPoint puduraya_pt = new GeoPoint(3145792,101701098);
+		OverlayItem puduraya_overlay = new OverlayItem(puduraya_pt, "Puduraya", "Kuala Lumpur");
+		itemizedoverlay.addOverlay(puduraya_overlay);
+
+		GeoPoint ktm_pt = new GeoPoint(3140021,101693133);
+		OverlayItem ktm_overlay = new OverlayItem(ktm_pt, "KTM", "Kuala Lumpur");
+		itemizedoverlay.addOverlay(ktm_overlay);
+
+		GeoPoint hentian_duta_pt = new GeoPoint(3171572,101674013);
+		OverlayItem hentian_duta_overlay = new OverlayItem(hentian_duta_pt, "Hentian Duta", "Kuala Lumpur");
+		itemizedoverlay.addOverlay(hentian_duta_overlay);
+
+		GeoPoint jalan_duta_pt = new GeoPoint(3176939,101657662);
+		OverlayItem jalan_duta_overlay = new OverlayItem(jalan_duta_pt, "Jalan Duta", "Kuala Lumpur");
+		itemizedoverlay.addOverlay(jalan_duta_overlay);
+
+		GeoPoint damansara_pt = new GeoPoint(3151031,101615107);
+		OverlayItem damansara_overlay = new OverlayItem(damansara_pt, "Damansara", "Kuala Lumpur");
+		itemizedoverlay.addOverlay(damansara_overlay);
+
+		GeoPoint puchong_pt = new GeoPoint(3035408,101616259);
+		OverlayItem puchong_overlay = new OverlayItem(puchong_pt, "Puchong Tesco", "Puchong");
+		itemizedoverlay.addOverlay(puchong_overlay);
+
+		mapOverlays.add(itemizedoverlay);
 	}
 
 	@Override
